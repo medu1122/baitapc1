de9e35bc1d2fe31a198dba65eed4d8a526106d73
medu1122 <79390838+medu1122@users.noreply.github.com>
2023-05-18 13:14:15 +0700

    Add files via upload

---
+++ bai1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime a = DateTime.Now;
+            Console.WriteLine("time hien tai : "+ a.ToString("dd/MM/yyyy HH:mm:ss"));
+            Console.ReadKey();
+        }
+    }
+}
