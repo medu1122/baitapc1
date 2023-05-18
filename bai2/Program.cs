7d7ffd6ef8f0c18a318afaf2a4bbcaae9d4d0ac3
medu1122 <79390838+medu1122@users.noreply.github.com>
2023-05-18 13:24:14 +0700

    Add files via upload

---
+++ bai2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float w; float h;
+            Console.Write("height = ");
+            w= float.Parse(Console.ReadLine());
+            Console.Write("width = ");
+            h = float.Parse(Console.ReadLine());
+            float area =w*h;
+            Console.WriteLine("area = " +area);
+            Console.ReadKey();
+        }
+    }
+}
