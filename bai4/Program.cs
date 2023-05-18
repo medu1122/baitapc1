72b3159b21695ef872345e2a407fe8f853856d60
medu1122 <79390838+medu1122@users.noreply.github.com>
2023-05-18 13:46:06 +0700

    Add files via upload

---
+++ bai4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap so tien ( 1 usd = 23000 vnd ) : ");
+            const float rate = 23000;
+            float usd = float.Parse(Console.ReadLine());
+            Console.WriteLine("VND = "+rate*usd );
+            Console.ReadKey();
+        }
+    }
+}
