fa62d6ab14d8eeb261a1d4af3e14dc280a83592c
medu1122 <79390838+medu1122@users.noreply.github.com>
2023-05-18 13:36:39 +0700

    Add files via upload

---
+++ bai3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write("Your name : ");
+            String name=Console.ReadLine();
+            Console.WriteLine("HI !!" +name);
+            Console.ReadKey();
+        }
+    }
+}
